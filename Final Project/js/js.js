61de9b9744eb67722b6e15b967e45f49a0e9696a
Marta Dzoba <marta.dzoba2001@gmail.com>
2021-05-05 22:33:26 +0300

    done final project

---
+++ Final Project/js/js.js
@@ -0,0 +1,201 @@
+var block = document.getElementById('plv');
+var car = document.getElementById('pls');
+block.onclick = function() {
+    this.style.display = "none";
+    car.style.display = "block"
+}
+
+car.onclick = function() {
+    this.style.display = "none";
+    block.style.display = "block";
+}
+var pp = document.getElementById('main')
+pp.onclick = function() {
+    this.style.color = "#F58122"
+}
+
+var pp = document.getElementById('program')
+pp.onclick = function() {
+    this.style.color = "#F58122"
+
+}
+var pp = document.getElementById('playlist')
+pp.onclick = function() {
+    this.style.color = "#F58122"
+
+}
+var pp = document.getElementById('cooperation')
+pp.onclick = function() {
+    this.style.color = "#F58122"
+
+}
+var pp = document.getElementById('order')
+pp.onclick = function() {
+    this.style.color = "#F58122"
+
+}
+var pp = document.getElementById('tobay')
+pp.onclick = function() {
+    this.style.color = "#F58122"
+
+}
+
+var bl = document.getElementById('bp');
+var cars = document.getElementById('bg');
+bl.onclick = function() {
+    this.style.display = "none";
+    cars.style.display = "block"
+}
+
+cars.onclick = function() {
+    this.style.display = "none";
+    bl.style.display = "block";
+}
+var play = document.getElementById('mt');
+var paus = document.getElementById('mp');
+play.onclick = function() {
+    this.style.display = "none";
+    paus.style.display = "block"
+}
+
+paus.onclick = function() {
+    this.style.display = "none";
+    play.style.display = "block";
+}
+var imgcard1 = document.getElementById('cq');
+var card1 = document.getElementById('cw');
+imgcard1.onclick = function() {
+    this.style.display = "none";
+    card1.style.display = "block"
+}
+
+card1.onclick = function() {
+    this.style.display = "none";
+    imgcard1.style.display = "block";
+}
+var imgcard2 = document.getElementById('ce');
+var card2 = document.getElementById('cr');
+imgcard2.onclick = function() {
+    this.style.display = "none";
+    card2.style.display = "block"
+}
+
+card2.onclick = function() {
+    this.style.display = "none";
+    imgcard2.style.display = "block";
+}
+var imgcard3 = document.getElementById('ct');
+var card3 = document.getElementById('cy');
+imgcard3.onclick = function() {
+    this.style.display = "none";
+    card3.style.display = "block"
+}
+
+card3.onclick = function() {
+    this.style.display = "none";
+    imgcard3.style.display = "block";
+}
+var imgcard4 = document.getElementById('cu');
+var card4 = document.getElementById('ci');
+imgcard4.onclick = function() {
+    this.style.display = "none";
+    card4.style.display = "block"
+}
+
+card4.onclick = function() {
+    this.style.display = "none";
+    imgcard4.style.display = "block";
+}
+var imgcard5 = document.getElementById('co');
+var card5 = document.getElementById('cp');
+imgcard5.onclick = function() {
+    this.style.display = "none";
+    card5.style.display = "block"
+}
+
+card5.onclick = function() {
+    this.style.display = "none";
+    imgcard5.style.display = "block";
+}
+var imgcard6 = document.getElementById('ca');
+var card6 = document.getElementById('cs');
+imgcard6.onclick = function() {
+    this.style.display = "none";
+    card6.style.display = "block"
+}
+
+card6.onclick = function() {
+    this.style.display = "none";
+    imgcard6.style.display = "block";
+}
+var imgcard7 = document.getElementById('cd');
+var card7 = document.getElementById('cf');
+imgcard7.onclick = function() {
+    this.style.display = "none";
+    card7.style.display = "block"
+}
+
+card7.onclick = function() {
+    this.style.display = "none";
+    imgcard7.style.display = "block";
+}
+var imgcard8 = document.getElementById('cg');
+var card8 = document.getElementById('ch');
+imgcard8.onclick = function() {
+    this.style.display = "none";
+    card8.style.display = "block"
+}
+
+card8.onclick = function() {
+    this.style.display = "none";
+    imgcard8.style.display = "block";
+}
+var footer1 = document.getElementById('f1');
+var footer2 = document.getElementById('f2');
+footer1.onclick = function() {
+    this.style.display = "none";
+    footer2.style.display = "block"
+}
+
+footer2.onclick = function() {
+    this.style.display = "none";
+    footer1.style.display = "block";
+}
+
+function ra() {
+    var val = $('.ranges').val();
+    $('.ranges').css({ 'background': '-webkit-linear-gradient(left, #F58122 0%, #F58122 ' + val + '%, #E9E9E9 ' + val + '%, #E9E9E9 100%)' });
+}
+var lang1 = document.getElementById('ENG');
+var lang2 = document.getElementById('UKR');
+lang1.onclick = function() {
+    this.style.display = "none";
+    lang2.style.display = "block"
+}
+
+lang2.onclick = function() {
+    this.style.display = "none";
+    lang1.style.display = "block";
+}
+var img1 = document.getElementById('1');
+var img2 = document.getElementById('c3');
+img1.onclick = function() {
+    this.style.display = "none";
+    img2.style.display = "block"
+}
+
+img2.onclick = function() {
+    this.style.display = "none";
+    img1.style.display = "block";
+}
+var img3 = document.getElementById('2');
+var img4 = document.getElementById('c4');
+img3.onclick = function() {
+    this.style.display = "none";
+    img4.style.display = "block"
+}
+
+img4.onclick = function() {
+    this.style.display = "none";
+    img3.style.display = "block";
+}
